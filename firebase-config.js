f4c8315c9bf2e447b4e34948e0b965bc0612dde9
JAEYOUNG PARK <jyhome1228@gmail.com>
2026-07-31 12:15:01 +0900

    Add Firebase web configuration

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js';
+import { getAuth } from 'https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDdEz9iZuHU2tP66_A_zGyDYWKaiqGQdCA',
+  authDomain: 'wavelab-5aa38.firebaseapp.com',
+  projectId: 'wavelab-5aa38',
+  storageBucket: 'wavelab-5aa38.firebasestorage.app',
+  messagingSenderId: '411115916892',
+  appId: '1:411115916892:web:f13e95a30b6d724c5be099'
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
